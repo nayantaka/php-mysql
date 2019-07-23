d3720051cdd369e833c8f6c8f6e38eae08a0dbd1
nayantaka <nayantaka@gmail.com>
2019-07-23 20:54:53 +0700

    Update input.php

--- input.php
+++ input.php
@@ -2,7 +2,29 @@
 <hr>
 <form action='simpan.php' method='post'>
 Nama Hero : <input type='text' name='nama'> <br>
-Tipe Hero : <input type='text' name='tipe'> <br>
+Tipe Hero : 
+<?php
+ $koneksi = new mysqli("localhost", "root", "", "ml");
+ $qry = "SELECT * FROM tipe";
+ $hasil = $koneksi->query($qry);
+ ?>
+<select name="tipe">
+ <?php
+	while($row = $hasil->fetch_array(MYSQLI_ASSOC)){
+	?>
+  <option value="<?php echo $row['id_tipe'];?>">
+  <?php
+	echo $row['nama_tipe'];
+  ?>
+  </option>
+  <?php
+	}
+	?>
+</select>
+
+
+
+ <br>
 Jenis Kelamin : <input type='text' name='jenkel'> <br>
 <input type="submit" value='simpan'>
 </form>
